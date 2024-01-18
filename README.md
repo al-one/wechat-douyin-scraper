4479adcc606c959819ae38122987be60cecc6a6f
Alone <hi@anlo.ng>
2024-01-18 15:05:39 +0800

    📺 Add douyin scraper plugin

--- README.md
+++ README.md
@@ -22,6 +22,7 @@
       "command": ["复制打开抖音", "v.douyin", "tiktok.com", "kuaishou.com"],
       "with_link": "{desc}\n高清视频链接:\n{link}", # string或bool，为string时作为回复模板
       "only_link": true,     # 仅发送链接
+      "without_at": true,    # 无需@机器人也会解析
       "keep_assets_days": 3  # 清理N天前的视频缓存
     }
   ]
--- plugin.py
+++ plugin.py
@@ -24,21 +24,54 @@ class App(Plugin):
         return cmds
 
     def did_receive_message(self, event: Event):
+        if self.config.get('without_at'):
+            self.reply(event)
+
         self.clear_assets()
 
     def will_generate_reply(self, event: Event):
-        query = event.context.query
-        for cmd in self.commands:
-            if cmd in query:
-                event.reply = self.reply(event)
-                event.bypass()
-                return
+        if not self.config.get('without_at'):
+            self.reply(event)
 
     def will_decorate_reply(self, event: Event):
         pass
 
     def will_send_reply(self, event: Event):
         pass
+
+    def reply(self, event: Event):
+        query = event.message.content
+        for cmd in self.commands:
+            if cmd in query:
+                event.reply = self.generate_reply(event)
+                event.bypass()
+                return
+
+    def generate_reply(self, event: Event) -> Reply:
+        query = event.message.content
+        result = asyncio.run(self.hybrid_parsing(query)) or {}
+        vdata = result.get('video_data') or result
+        url = vdata.get('nwm_video_url') or vdata.get('video_url')
+        logger.info('Scraper result: %s', url or result)
+        if not url:
+            if msg := result.get('message'):
+                return Reply(ReplyType.TEXT, f'Scraper: {msg}')
+            return Reply(ReplyType.TEXT, f'{result}' or '获取视频失败')
+
+        with_link = self.config.get('with_link')
+        only_link = self.config.get('only_link')
+        if with_link or only_link:
+            link = vdata.get('nwm_video_url_HQ') or url
+            if with_link not in [True, 1, '1', 'on', 'yes', 'true']:
+                if '{link}' not in with_link:
+                    with_link = f'{with_link}' + '{link}'
+                link = with_link.replace('{link}', link)
+                link = link.replace('{desc}', result.get('desc', ''))
+            reply = Reply(ReplyType.TEXT, link.strip())
+            if only_link:
+                return reply
+            event.channel.send(reply, event.message)
+        return Reply(ReplyType.VIDEO, url)
 
     async def hybrid_parsing(self, url):
         result = {}
@@ -50,31 +83,6 @@ class App(Plugin):
                 result = {'message': f'{exc}'}
                 await asyncio.sleep(0.1)
         return result
-
-    def reply(self, event: Event) -> Reply:
-        query = event.context.query
-        result = asyncio.run(self.hybrid_parsing(query)) or {}
-        vdata = result.get('video_data') or result
-        url = vdata.get('nwm_video_url') or vdata.get('video_url')
-        logger.info('Scraper result: %s', url or result)
-        if url:
-            with_link = self.config.get('with_link')
-            only_link = self.config.get('only_link')
-            if with_link or only_link:
-                link = vdata.get('nwm_video_url_HQ') or url
-                if with_link not in [True, 1, '1', 'on', 'yes', 'true']:
-                    if '{link}' not in with_link:
-                        with_link = f'{with_link}' + '{link}'
-                    link = with_link.replace('{link}', link)
-                    link = link.replace('{desc}', result.get('desc', ''))
-                reply = Reply(ReplyType.TEXT, link.strip())
-                if only_link:
-                    return reply
-                event.channel.send(reply, event.message)
-            return Reply(ReplyType.VIDEO, url)
-        if msg := result.get('message'):
-            return Reply(ReplyType.TEXT, f'Scraper: {msg}')
-        return Reply(ReplyType.TEXT, f'{result}' or '获取失败')
 
     def clear_assets(self):
         now = time.time()
