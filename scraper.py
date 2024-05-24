90448efdba7e4e46b4f473e8b49d14752d7043a9
Alone <hi@anlo.ng>
2024-05-24 14:37:24 +0800

    📺 improve douyin scraper

--- scraper.py
+++ scraper.py
@@ -378,7 +378,7 @@ class Scraper:
         """
         try:
             # 构造访问链接/Construct the access link
-            api_url = "https://beta.tikhub.io/api/v1/douyin/web/fetch_one_video?aweme_id=%s" % video_id
+            api_url = "https://beta.tikhub.io/api/v1/douyin/app/v3/fetch_one_video?aweme_id=%s" % video_id
             #api_url = "https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids=%s&a_bogus=64745b2b5bdc4e75b720a9a85b19867a" % video_id
             #api_url = "https://www.douyin.com/aweme/v1/web/aweme/detail/?device_platform=webapp&aid=6383&channel=channel_pc_web&aweme_id=%s&pc_client_type=1&version_code=190500&version_name=19.5.0&cookie_enabled=true&screen_width=1344&screen_height=756&browser_language=zh-CN&browser_platform=Win32&browser_name=Firefox&browser_version=110.0&browser_online=true&engine_name=Gecko&engine_version=109.0&os_name=Windows&os_version=10&cpu_core_num=16&device_memory=&platform=PC&webid=7158288523463362079&msToken=abL8SeUTPa9-EToD8qfC7toScSADxpg6yLh2dbNcpWHzE0bT04txM_4UwquIcRvkRb9IU8sifwgM1Kwf1Lsld81o9Irt2_yNyUbbQPSUO8EfVlZJ_78FckDFnwVBVUVK"  % video_id
             # api_url = self.generate_x_bogus_url(api_url)
